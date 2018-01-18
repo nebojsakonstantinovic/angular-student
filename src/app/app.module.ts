96f50318cf5d3179c2a84caf82dcfaf222066af5
Frontend <frontend@Cubess-iMac-3.local>
2018-01-18 16:00:37 +0100

    inicijalni commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
